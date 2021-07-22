441eb5452c656ba4dfaa5af4739de2b250c5e3c8
jacobkeller02 <keller02.jk@gmail.com>
2021-07-21 20:45:29 -0400

    Fix NPM modules by doing NPM install

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. hello. hi hi HI HI HI HI HI
+          Edit <code>src/App.js</code> and save to reload. hello. BUBBLES!!!!!
         </p>
         <a
           className="App-link"
